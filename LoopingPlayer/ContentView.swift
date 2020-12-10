de608974a4b6543412fee3f138923c5614fa9e57
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-12-10 12:44:23 +0200

    Initial Commit

---
+++ LoopingPlayer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LoopingPlayer
+//
+//  Created by Ramill Ibragimov on 10.12.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LoopingPlayer/LoopingPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoopingPlayerApp.swift
+//  LoopingPlayer
+//
+//  Created by Ramill Ibragimov on 10.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct LoopingPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
